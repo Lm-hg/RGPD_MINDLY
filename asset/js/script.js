18062c7f21f47abb07c64026c89d6546d93c9d16
Lm-hg <you@example.com>
2025-05-01 23:40:03 +0200

    mise à place du rgpd mindly

---
+++ asset/js/script.js
@@ -0,0 +1,34 @@
+const url = "../asset/rgpd/Politique-de-confidentialite-rgpd-1.pdf"; 
+const pdfContainer = document.getElementById("pdf-container");
+const paymentBanner = document.getElementById("payment-banner");
+const linkSuite = document.getElementById("link-suite");
+
+
+
+pdfjsLib.getDocument(url).promise.then(pdf => {
+    const totalPages = pdf.numPages;
+    const maxFreePages = 14;
+
+    for (let i = 1; i <= totalPages; i++) {
+        if (i > maxFreePages) {
+            paymentBanner.style.display = "block"; 
+            break;
+        }
+
+        pdf.getPage(i).then(page => {
+            const viewport = page.getViewport({ scale: 1 });
+            const canvas = document.createElement("canvas");
+            const context = canvas.getContext("2d");
+            canvas.width = viewport.width;
+            canvas.height = viewport.height;
+
+            pdfContainer.appendChild(canvas);
+
+            page.render({
+                canvasContext: context,
+                viewport: viewport
+            });
+        });
+    }
+});
+
